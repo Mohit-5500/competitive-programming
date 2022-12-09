0a2d61b7ad3cbd253719f895ca546d8fb2eb0274
Mohit-5500 <94555314+Mohit-5500@users.noreply.github.com>
2022-12-09 17:52:18 +0530

    Add files via upload

---
+++ FloorOfNumber.py
@@ -0,0 +1,18 @@
+def binarysearch(arr,target):
+    start=0
+    end=len(arr)-1
+    mid=0
+    while(start<=end):
+        mid=(start+end)//2
+
+        if(target>arr[mid]):
+            start=mid+1
+        elif (target<arr[mid]):
+            end=mid-1
+        else:
+            return mid
+    return end
+arr=[2,3,5,9,14,16,18]
+target=15
+result=binarysearch(arr,target)
+print(f"Floor number of target is {result}")
